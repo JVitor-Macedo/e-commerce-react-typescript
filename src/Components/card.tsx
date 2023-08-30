eecd6818e7379020901edd178d39a48d53fda1b7
Vitor Macedo <14vitorjo@gmail.com>
2023-08-30 15:54:19 -0300

    ok

--- src/Components/card.tsx
+++ src/Components/card.tsx
@@ -1,7 +1,28 @@
+import { AiFillStar } from "react-icons/ai"
+import { BsFillBagHeartFill } from "react-icons/bs"
+
 function Card() {
-    return <div>
-        Buttons
+    return <section className="card">
+    <img className="card-img" src="https://down-br.img.susercontent.com/file/sg-11134201-7qvcv-lf0yt1m9p6zy32_tn" alt="img-camisa" />
+    <div className="card-details">
+        <h3 className="card-title">Camisa</h3>
+        <section className="card-reviews">
+            <AiFillStar className="rating-star"/> 
+            <AiFillStar className="rating-star"/> 
+            <AiFillStar className="rating-star"/> 
+            <AiFillStar className="rating-star"/>
+            <span className="total-reviews">4</span>
+        </section>
+        <section className="card=price">
+            <div className="price">
+                <del>R$ 300</del> 200
+            </div>
+            <div className="bag">
+                <BsFillBagHeartFill className="bag-icon"/>
+            </div>
+        </section>
     </div>
+</section>
 }
 
 export default Card
